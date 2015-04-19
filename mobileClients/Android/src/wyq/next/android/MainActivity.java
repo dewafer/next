dbb21455390d84620726fc207562c600b28fe5f7
dewafer <dewafer@gmail.com>
2015-04-19 21:02:39 +0800

    add post invalidate for image view

--- mobileClients/Android/src/wyq/next/android/MainActivity.java
+++ mobileClients/Android/src/wyq/next/android/MainActivity.java
@@ -130,6 +130,7 @@ public class MainActivity extends Activity {
 		@Override
 		protected void onPostExecute(Bitmap result) {
 			mImageView.setImageBitmap(result);
+			mImageView.postInvalidate();
 		}
 
 	}
