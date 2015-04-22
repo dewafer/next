8f951d85c44e2a552fe83907b2fbfa1a34221b53
dewafer <dewafer@gmail.com>
2015-04-22 23:48:06 +0800

    大量更新
    1.增加了NextApplication，并且将一些功能全部移动到这个类中。
    2.增加了缓存机制，下载的图片会以缓存文件的形式保存在本地cache目录下。
    3.增加了图片下载时的转圈圈进度条。

--- mobileClients/Android/Next/src/wyq/next/android/MainActivity.java
+++ mobileClients/Android/Next/src/wyq/next/android/MainActivity.java
@@ -1,14 +1,18 @@
 package wyq.next.android;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.TypedValue;
@@ -19,6 +23,10 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -27,11 +35,12 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		// 使用gridView来展示图片
-		// 参考Android Develop Guide中User Interface中的Grid View
-		// 使用ImageAdapter来加载图片
-		GridView gridview = (GridView) findViewById(R.id.gridView);
+		// gridview使用ImageAdapter适配器来获得各个子view
+		gridview = (GridView) findViewById(R.id.gridView);
 		gridview.setAdapter(new ImageAdapter(this));
+
+		// 加载图片列表
+		((NextApplication) getApplication()).loadImageUrls(gridview);
 	}
 
 	@Override
@@ -47,12 +56,44 @@ public class MainActivity extends Activity {
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
+		if (id == R.id.action_refresh) {
+			// 都手动刷新了，能不清除cache么？
+			getNextApplication().getThumbCache().clear();
+			// 加载图片列表
+			getNextApplication().loadImageUrls(gridview);
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
 	}
 
+	public NextApplication getNextApplication() {
+		return (NextApplication) getApplication();
+	}
+
+	// 从NextApplication中取得thumbUrls的list
+	protected List<String> getThumbUrls() {
+		return getNextApplication().getThumbUrls();
+	}
+
+	// 从NextApplication中取得cache文件的map
+	protected Map<String, String> getThumbCache() {
+		return getNextApplication().getThumbCache();
+	}
+
+	// 读cache文件到bitmap
+	protected Bitmap readCacheFile(String fileName) {
+		return getNextApplication().readCacheFile(fileName);
+	}
+
+	// 把inputStream写到cache目录中的fileName文件
+	protected void saveImageAsCacheFile(String fileName, InputStream is)
+			throws FileNotFoundException, IOException {
+		getNextApplication().saveImageAsCacheFile(fileName, is);
+	}
+
+	/**
+	 * ImageAdapter适配器，用来给gridView填充图片
+	 */
 	public class ImageAdapter extends BaseAdapter {
 		private Context mContext;
 
@@ -61,7 +102,7 @@ public class MainActivity extends Activity {
 		}
 
 		public int getCount() {
-			return mThumbUrl.length;
+			return getThumbUrls().size();
 		}
 
 		public Object getItem(int position) {
@@ -72,65 +113,141 @@ public class MainActivity extends Activity {
 			return 0;
 		}
 
-		// create a new ImageView for each item referenced by the Adapter
 		public View getView(int position, View convertView, ViewGroup parent) {
-			ImageView imageView;
-			if (convertView == null) { // if it's not recycled, initialize some
-										// attributes
-				imageView = new ImageView(mContext);
-				imageView.setLayoutParams(new GridView.LayoutParams(dp2pix(85),
+			RelativeLayout view;
+
+			// convertView是循环使用的view
+			// 如果为null则表明该view没有循环使用，创建新view
+			if (convertView == null) {
+				// 使用relativeLayout作为子view
+				// 85dp x 85dp大小
+				view = new RelativeLayout(mContext);
+				view.setLayoutParams(new GridView.LayoutParams(dp2pix(85),
 						dp2pix(85)));
-				imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-				imageView
-						.setPadding(dp2pix(8), dp2pix(8), dp2pix(8), dp2pix(8));
 			} else {
-				imageView = (ImageView) convertView;
+				// 循环使用
+				view = (RelativeLayout) convertView;
+				view.removeAllViews();
 			}
 
-			DownloadImageTask task = new DownloadImageTask(imageView);
-			task.execute(mThumbUrl[position]);
-			return imageView;
-		}
+			// relativeLayout里面放入一个progressBar表明正在加载
+			ProgressBar progressBar = new ProgressBar(mContext);
+			// 设置progressBar表示为无限循环
+			progressBar.setIndeterminate(true);
+			// 正常大小并且居中
+			LayoutParams params = new RelativeLayout.LayoutParams(
+					LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+			params.addRule(RelativeLayout.CENTER_IN_PARENT);
+			progressBar.setLayoutParams(params);
 
+			// progressBar加入relativeLayout中
+			view.addView(progressBar);
+
+			// 开启下载任务
+			DownloadImageTask task = new DownloadImageTask(getThumbUrls().get(
+					position), view);
+			task.execute();
+
+			// view返回到画面上显示
+			return view;
+		}
 	}
 
-	// 图片的url
-	private String[] mThumbUrl = {
-			"http://ww2.sinaimg.cn/large/51d3f408gw1eqzld8ueetj207407ggln.jpg",
-			"http://ww1.sinaimg.cn/large/51d3f408gw1eqzkqh3fhag20az06u1kz.gif",
-			"http://ww3.sinaimg.cn/large/51d3f408gw1eqzld9mg0bj20dw0afaaj.jpg",
-			"http://ww1.sinaimg.cn/large/51d3f408gw1eqzlda4swgj20ba0cgjrn.jpg" };
+	private GridView gridview;
 
 	// 因为在mainThread上不能进行网络操作，所以使用AsyncTask来加载图片
-	class DownloadImageTask extends AsyncTask<String, Object, Bitmap> {
+	class DownloadImageTask extends AsyncTask<Object, Object, Bitmap> {
 
-		private ImageView mImageView;
+		// 这个view用来装图片子imageView
+		// 理论上应该是relativeView，但在这里我们不需要知道是什么layout
+		// 所以用父类ViewGroup
+		private ViewGroup mParentView;
+		private String mImageUrl;
 
-		public DownloadImageTask(ImageView mImageView) {
-			this.mImageView = mImageView;
+		public DownloadImageTask(String mImageUrl, ViewGroup mParentView) {
+			this.mImageUrl = mImageUrl;
+			this.mParentView = mParentView;
 		}
 
 		@Override
-		protected Bitmap doInBackground(String... params) {
+		protected Bitmap doInBackground(Object... params) {
+
+			Map<String, String> cache = getThumbCache();
+			if (cache.containsKey(mImageUrl)) {
+				// 想啥呢？！如果没cache，每次都去下载表累死我啊
+				// 如果有cache就从cache里拿
+				String cacheFileName = cache.get(mImageUrl);
+				return readCacheFile(cacheFileName);
+			}
+
+			// 开始下载
 			Bitmap bitmap = null;
 			try {
-				URL image = new URL(params[0]);
+				// 加载图片
+				URL image = new URL(mImageUrl);
 				HttpURLConnection conn = (HttpURLConnection) image
 						.openConnection();
 				conn.connect();
-				bitmap = BitmapFactory.decodeStream(conn.getInputStream());
+				// 用UUID作为文件名
+				String fileName = UUID.randomUUID().toString();
+				// 图片下载后写成cache文件
+				saveImageAsCacheFile(fileName, conn.getInputStream());
+				// 再把cache文件解码成bitmap
+				bitmap = readCacheFile(fileName);
+				// cache文件读取成功的话
+				if (bitmap != null) {
+					// 保存cache
+					cache.put(mImageUrl, fileName);
+				}
 			} catch (MalformedURLException e) {
 				e.printStackTrace();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+			// 如果没有搞到图片则会返回null
 			return bitmap;
 		}
 
 		@Override
 		protected void onPostExecute(Bitmap result) {
-			mImageView.setImageBitmap(result);
-			mImageView.postInvalidate();
+			View resultView;
+
+			if (result == null) {
+				// 没有搞到图片肿木板？
+				// 放一个textView写上
+				// 木有图片
+				TextView textView = new TextView(mParentView.getContext());
+				LayoutParams params = new RelativeLayout.LayoutParams(
+						LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+				params.addRule(RelativeLayout.CENTER_IN_PARENT);
+				textView.setLayoutParams(params);
+
+				textView.setText(R.string.no_image);
+
+				resultView = textView;
+			} else {
+
+				// 搞到图片了
+				// 设置好居中及大小后
+				// 显示
+				ImageView imageView = new ImageView(mParentView.getContext());
+				LayoutParams params = new RelativeLayout.LayoutParams(
+						LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+				params.addRule(RelativeLayout.CENTER_IN_PARENT);
+				imageView.setLayoutParams(params);
+				imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+				imageView
+						.setPadding(dp2pix(8), dp2pix(8), dp2pix(8), dp2pix(8));
+				imageView.setImageBitmap(result);
+
+				resultView = imageView;
+			}
+
+			// 父viewGroup中的子view都删了吧
+			// 然后才能正常显示啊
+			mParentView.removeAllViews();
+			mParentView.addView(resultView);
+			mParentView.postInvalidate();
 		}
 
 	}
---
+++ mobileClients/Android/Next/src/wyq/next/android/NextApplication.java
@@ -0,0 +1,124 @@
+package wyq.next.android;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import android.app.Application;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.widget.AbsListView;
+
+public class NextApplication extends Application {
+
+	private final List<String> thumbUrls = new ArrayList<String>();
+	// 图片的cache，key是URL，value是对应的文件名（不包含路径）
+	private final Map<String, String> thumbCache = new HashMap<String, String>();
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+		// app启动的时候去加载图片的url
+		// 算了，还是MainActivity起来的时候再去加载吧
+		// loadImageUrls(null);
+	}
+
+	public void loadImageUrls(AbsListView gridview) {
+		// TODO 这个方法将会去服务器把图片的URL地址给拉下来并更新thumbUrls
+		// 传入一个gridView，保证url拉完之后去更新这个gridView
+		// 当然如果你传个null我也没办法
+		// 确保这货是异步执行的
+		new DownloadImageUrlsTask(gridview).execute();
+	}
+
+	// 把inputStream写到cache目录中的fileName文件
+	public void saveImageAsCacheFile(String fileName, InputStream is)
+			throws FileNotFoundException, IOException {
+
+		File cache = new File(getCacheDir(), fileName);
+		FileOutputStream out = null;
+		BufferedInputStream in = null;
+
+		try {
+			out = new FileOutputStream(cache);
+			in = new BufferedInputStream(is);
+			int len = 4086;
+			byte[] buffer = new byte[len];
+			while ((len = in.read(buffer, 0, len)) != -1) {
+				out.write(buffer, 0, len);
+			}
+		} catch (FileNotFoundException e) {
+			throw e;
+		} catch (IOException e) {
+			throw e;
+		} finally {
+			try {
+				if (out != null) {
+					out.flush();
+					out.close();
+				}
+				if (in != null) {
+					in.close();
+				}
+			} catch (IOException e) {
+				throw e;
+			}
+		}
+	}
+
+	// 读cache文件到bitmap
+	public Bitmap readCacheFile(String fileName) {
+		File cache = new File(getCacheDir(), fileName);
+		return BitmapFactory.decodeFile(cache.getPath());
+	}
+
+	class DownloadImageUrlsTask extends AsyncTask<Object, Object, String[]> {
+		private AbsListView mContainer;
+
+		public DownloadImageUrlsTask(AbsListView parentView) {
+			this.mContainer = parentView;
+		}
+
+		@Override
+		protected String[] doInBackground(Object... params) {
+			// TODO 正真地拉是在这里拉，我不是说拉屎
+			// 临时地用一个固定的数组来替代一下
+			return new String[] {
+					"http://ww2.sinaimg.cn/large/51d3f408gw1eqzld8ueetj207407ggln.jpg",
+					"http://ww1.sinaimg.cn/large/51d3f408gw1eqzkqh3fhag20az06u1kz.gif",
+					"http://ww3.sinaimg.cn/large/51d3f408gw1eqzld9mg0bj20dw0afaaj.jpg",
+					"http://ww1.sinaimg.cn/large/51d3f408gw1eqzlda4swgj20ba0cgjrn.jpg" };
+		}
+
+		@Override
+		protected void onPostExecute(String[] result) {
+			thumbUrls.clear();
+			Collections.addAll(thumbUrls, result);
+			if (mContainer != null) {
+				// 让装载图片的视图经行更新
+				// 刷新按钮使每个子view失效
+				mContainer.invalidateViews();
+			}
+		}
+
+	}
+
+	public List<String> getThumbUrls() {
+		return thumbUrls;
+	}
+
+	public Map<String, String> getThumbCache() {
+		return thumbCache;
+	}
+
+}
